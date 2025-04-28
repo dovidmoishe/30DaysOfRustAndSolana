c6865e3772e63a43585565235989d2b1d259b1d7
David Baiye <ozerlihashem@gmail.com>
2025-04-28 13:12:58 +0100

    Lets get going

---
+++ week1/target/debug/deps/week1-7568f2f427092cf4.d
@@ -0,0 +1,5 @@
+/home/dave/Desktop/30DaysOfRust/week1/target/debug/deps/libweek1-7568f2f427092cf4.rmeta: src/main.rs
+
+/home/dave/Desktop/30DaysOfRust/week1/target/debug/deps/week1-7568f2f427092cf4.d: src/main.rs
+
+src/main.rs:
---
+++ week1/target/debug/deps/week1-c952ab95ba4dda41.d
@@ -0,0 +1,5 @@
+/home/dave/Desktop/30DaysOfRust/week1/target/debug/deps/libweek1-c952ab95ba4dda41.rmeta: src/main.rs
+
+/home/dave/Desktop/30DaysOfRust/week1/target/debug/deps/week1-c952ab95ba4dda41.d: src/main.rs
+
+src/main.rs:
