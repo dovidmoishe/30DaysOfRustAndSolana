e501fb64b27dbb95b368ba3a9419c3973aee672a
David Baiye <ozerlihashem@gmail.com>
2025-05-02 15:02:06 +0100

    Push Readme + gitignore

---
+++ readme.md
@@ -0,0 +1,7 @@
+
+# 30 Days of Rust and Solana development
+
+This is a challenge I started on the 1st of May to make me a solana smart contract developer in 30 days. 
+
+![Rust](https://pbs.twimg.com/media/Gpkus8lWAAAjEDN.jpg)
+
--- week1/src/main.rs
+++ week1/src/main.rs
@@ -1,3 +1,30 @@
+fn sum(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
+fn collatz_length(mut n: i32) -> u32 {
+    let mut len = 1;
+    while n > 1 {
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+        len+= 1;
+    }
+    len
+}
 fn main() {
-    println!("Hello, world!");
+    struct User {
+        name: String,
+        age: i32,
+    }
+    let username = String::from("Dvaid");
+    let david: User = User {name: username, age: 19};
+    println!("{}", david.name);
+    let sumoftwonumbers: i32 = sum(1, 2);
+    for x in 1..5 {
+        dbg!(x);
+    }
+    if sumoftwonumbers < 5 {
+        println!("Sum is less than 5");
+    }
+    println!("Length: {}", collatz_length(11));
+    println!("Hello, world! {sumoftwonumbers}");
 }
